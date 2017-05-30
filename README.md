80577dbea849a36b80e65b34dea59a04fe088851
Hiroki Ishiwatari <ishiwatari@fragmentof.net>
2017-05-31 02:54:03 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# gulp-tumblr-theme-parser
+
+[Node tumblr theme parser](https://github.com/carrot/tumblr-theme-parser) for [gulpjs/gulp: The streaming build system](https://github.com/gulpjs/gulp)
+
+# Basic Usage
+
+```
+'use strict';
+
+var gulp = require('gulp');
+var parser = require('gulp-tumblr-theme-parser');
+
+gulp.task('html', function() {
+  return gulp.src('./index.html')
+    .pipe(parser({ data: './data.json' }))
+    .pipe(gulp.dest('./build'));
+});
+```
+
+# Options
+
+## data
+This property can still pass an object as a file path.
+
+```
+{ data: './data.json' }
+
+// or
+
+{ data: { 'title': 'Hello, world.' } }
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict'
+
+var deap = require('deap');
+var fs = require('fs');
+var gutil = require('gulp-util');
+var isObject = require('isobject');
+var path = require('path');
+var parser = require('tumblr-theme-parser');
+var through = require('through2');
+
+var PLUGIN_NAME = 'gulp-tumblr-theme-parser';
+
+module.exports = function(opts, cb) {
+  opts = opts ? opts : {};
+
+  function setup(opts, cb) {
+    var options = deap({}, opts);
+
+    if (options.data && !isObject(options.data)) {
+      fs.readFile(options.data, function (err, file) {
+        options.data = JSON.parse(String(file));
+        cb(options);
+      });
+    } else {
+      cb(options);
+    }
+  }
+
+  function compileHtml(file, enc, cb) {
+    setup(opts || {}, compile);
+
+    function compile(opts) {
+      var originalContents = String(file.contents);
+      var data = opts.data || {};
+
+      if (file.isNull() || !file.contents.toString()) {
+        cb(null, file);
+        return;
+      }
+
+      try {
+        var compiled = parser.compile(originalContents, data);
+        file.contents = new Buffer(compiled);
+        file.path = path.join(file.base, file.relative)
+      } catch (err) {
+        error(err);
+      }
+
+      function error (err) {
+        cb(new gutil.PluginError(PLUGIN_NAME, err, {fileName: file.path}));
+        this.emit('error', new gutil.PluginError(PLUGIN_NAME, err));
+      }
+
+      cb(null, file);
+    }
+  }
+
+  return through.obj(compileHtml);
+};
